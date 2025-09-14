d2aed1113e9f754f9022ac71a25c3c6420b1911b
Abdulhaadi1234 <abdulhaadiahmad21@gmail.com>
2025-09-14 13:18:41 +0500

    introduction to turtle

---
+++ main4.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(300,400)
+p = turtle.Turtle()
+size = 0
+while True:
+    for i in range(4):
+        p.fd(100)
+        p.left(90)
+turtle.done()        
